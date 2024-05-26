443a3edfff44b98bab03cf4002007217dda72c36
Jean Pierre Veliz <jeanpierreveliz26@gmail.com>
2024-05-25 19:37:19 -0500

    analysisSong componet created

---
+++ src/pages/AppPage/components/AnalysisSong/AnalysisSong.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from 'react';
+import styles from './AnalysisSong.module.scss';
+
+export interface AnalysisSongProps {
+	// types...
+}
+
+const AnalysisSong: React.FC<AnalysisSongProps>  = ({}) => {
+	return (
+		<div className={styles.analysissong}>
+ 			AnalysisSong works!
+ 		</div>
+	);
+};
+
+export default AnalysisSong;
---
+++ src/pages/AppPage/components/AnalysisSong/index.ts
@@ -0,0 +1,1 @@
+export { default as AnalysisSong } from './AnalysisSong';
